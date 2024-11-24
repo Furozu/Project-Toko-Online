758d40aa982f374059c48906f94478ac5844ed0b
kenleoo <c14230067@john.petra.ac.id>
2024-11-24 20:01:42 +0700

    style 3

--- adminPage.php
+++ adminPage.php
@@ -73,9 +73,37 @@ session_start();
                 <p class="h4 p-10 bg-warning rounded fw-bold">CREATE NEW PRODUCT</p>
             </div>
             <div class="col-lg-3 justify-items-center"></div>
-
         </div>
     </section>
+    <!-- pop up create product -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="recipient-name" class="col-form-label">Recipient:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="form-group">
+                            <label for="message-text" class="col-form-label">Message:</label>
+                            <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Send message</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <section class="container mx-auto my-10">
         <?php

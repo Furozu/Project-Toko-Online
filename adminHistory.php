c793897164de37d37c49e84f92119d77964ff470
Furozu <nat80222@gmail.com>
2024-12-04 15:28:54 +0700

    added adminHistory

--- adminHistory.php
+++ adminHistory.php
@@ -62,7 +62,101 @@ session_start();
     </nav>
 
     <section>
+        <div class="container mx-auto py-6">
+            <h1 class="text-center text-2xl font-bold mb-4 text-gray-800">Transaction History</h1>
 
+            <div class="overflow-x-auto">
+                <table class="table-auto w-full text-sm text-left border border-gray-300 shadow-lg rounded-lg">
+                    <thead class="bg-gradient-to-r from-gray-600 to-black text-yellow-400 text-center">
+                        <tr>
+                            <th class="px-4 py-3">#</th>
+                            <th class="px-2 py-3">Date of Payment</th>
+                            <th class="px-4 py-3">User ID</th>
+                            <th class="px-4 py-3">Username</th>
+                            <th class="px-4 py-3 text-left">Items</th>
+                            <th class="px-5 py-3">Total Price</th>
+                            <th class="px-4 py-3">Payment Type</th>
+                            <th class="px-4 py-3">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 text-center">
+
+                        <?php
+                        $stmt = $mysqli->query("SELECT c.checkout_id, c.date, c.user_id, u.username, c.total_harga, p.payment_method, c.status
+                                                FROM checkout c
+                                                JOIN users u ON c.user_id = u.user_id
+                                                JOIN payment p ON c.payment_id = p.payment_id;");
+                        while ($row = $stmt->fetch_assoc()) {
+
+                            // checkout data
+                            $checkout = [
+                                'id' => htmlentities($row['checkout_id']),
+                                'date' => htmlentities($row['date']),
+                                'user_id' => htmlentities($row['user_id']),
+                                'username' => htmlentities($row['username']),
+                                'total_harga' => htmlentities($row['total_harga']),
+                                'payment_type' => htmlentities($row['payment_method']),
+                                'status' => htmlentities($row['status'])
+                            ];
+                        ?>
+
+                            <tr class="bg-gray-50 hover:bg-gradient-to-r from-gray-50 to-gray-200 transition-colors duration-300">
+                                <td class="px-4 py-3"><?= $checkout['id']; ?></td>
+                                <td class="px-4 py-3">2024-12-01</td>
+                                <td class="px-4 py-3"><?= $checkout['user_id']; ?></td>
+                                <td class="px-4 py-3 font-semibold text-blue-600"><?= $checkout['username']; ?></td>
+                                <td class="px-4 py-3">
+                                    <ul class="list-disc pl-5 text-left">
+
+                                        <?php
+                                        $stmt = $mysqli->query("SELECT d.detail_id, p.nama_product, d.jumlah_product, p.harga_satuan
+                                        FROM detailcheckout d
+                                        JOIN products p ON d.product_id = p.product_id;");
+
+                                        while ($row = $stmt->fetch_assoc()) {
+                                            $totalSatuan = $row['jumlah_product'] * $row['harga_satuan'];
+                                            echo '<li>' . $row['nama_product'] . ' (' . $row['jumlah_product'] . ' x Rp. ' . number_format($row['harga_satuan'], 0, ',', '.') . ') = Rp. ' . number_format($totalSatuan, 0, ',', '.') . '</li>';
+                                        }
+                                        ?>
+
+                                    </ul>
+                                </td>
+                                <td class="px-4 py-3 font-semibold text-green-600">Rp. <?= number_format($checkout['total_harga'], 0, ',', '.'); ?></td>
+                                <td class="px-4 py-3"><?= $checkout['payment_type']; ?></td>
+                                <td class="px-4 py-3">
+                                    <span class="px-2 py-1 bg-yellow-300 text-yellow-700 text-xs font-medium rounded-full"><?= $checkout['status']; ?></span>
+                                </td>
+                            </tr>
+
+                        <?php
+                        }
+                        ?>
+
+                        <!-- TEST -->
+                        <tr class="bg-gray-50">
+                            <td class="px-4 py-3">ID</td>
+                            <td class="px-4 py-3">Date</td>
+                            <td class="px-4 py-3">UserID</td>
+                            <td class="px-4 py-3 font-semibold text-blue-600">Test</td>
+                            <td class="px-4 py-3">
+                                <ul class="list-disc pl-5 text-left">
+                                    <li>Item C (3 x $8) = $24</li>
+                                    <li>Item D (1 x $25) = $25</li>
+                                </ul>
+                            </td>
+                            <td class="px-4 py-3 font-semibold text-green-600">$49</td>
+                            <td class="px-4 py-3">PaymentType</td>
+                            <td class="px-4 py-3">
+                                <span class="px-2 py-1 bg-green-300 text-green-700 text-xs font-medium rounded-full">Completed</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+
+        </div>
     </section>
 
 </body>
